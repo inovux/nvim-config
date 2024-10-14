dc4774fd6f10e1a0b17be0013fb87a1df860fe56
Stanley Thijssen <stanley.thijssen@gmail.com>
2024-10-14 15:18:20 +0800

    feat: added lsp configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,15 @@
 return {
-	-- init.lua:
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                              , branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	"theprimeagen/harpoon",
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
 	{'neovim/nvim-lspconfig'},
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
-	{ "rose-pine/neovim", name = "rose-pine" },
+	{'rose-pine/neovim', name = 'rose-pine'},
 }
