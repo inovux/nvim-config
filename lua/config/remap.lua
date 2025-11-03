246b6d9eda78077293ee245a86257c706a071a9d
Stanley Thijssen <stanley.thijssen@gmail.com>
2025-11-04 03:53:45 +0800

    feat: configure lsp remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -6,3 +6,6 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<C-d>", "<C-d>M")
 vim.keymap.set("n", "<C-u>", "<C-u>M")
+vim.keymap.set("n", "<C-h>", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "D", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n", "v" }, "<C-a>", vim.lsp.buf.code_action, {})
